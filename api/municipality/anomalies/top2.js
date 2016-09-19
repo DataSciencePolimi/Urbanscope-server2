53f3984db0199f74751a75a77e27c3d0c53e7962
Riccardo Volonterio <voloxxx@gmail.com>
2016-09-19 18:01:32 +0200

    feat: Added improved municipality queries

---
+++ api/municipality/anomalies/top2.js
@@ -0,0 +1,91 @@
+'use strict';
+// Load system modules
+
+// Load modules
+const co = require( 'co' );
+const _ = require( 'lodash' );
+const Boom = require( 'boom' );
+const moment = require( 'moment' );
+const debug = require( 'debug' )( 'UrbanScope:server:api:municipality:anomalies:top2' );
+
+// Load my modules
+const getAnomalies = require( '../../../utils/get-anomalies2' );
+const getDateBetween = require( '../../../utils/get-between' );
+
+// Constant declaration
+const DATE_FORMAT = require( '../../../config/' ).dateFormat;
+const MUNICIPALITIES = require( '../../../config/milan_municipalities.json' );
+const CACHE_MAX_AGE = 60*60*24; // 1 dd
+
+// Module variables declaration
+
+// Module functions declaration
+function* topDistrict( ctx ) {
+  // Cache MAX_AGE
+  ctx.maxAge = Infinity; // We can store past values forever
+
+  debug( 'Requested district' );
+
+  const start = ctx.startDate;
+  const end = ctx.endDate;
+  const limit = ctx.limit;
+  const language = ctx.language;
+
+
+  if( start.isAfter( end ) ) {
+    throw Boom.badRequest( 'Start date after end date' );
+  }
+
+  const response = {
+    startDate: start.format( DATE_FORMAT ),
+    endDate: end.format( DATE_FORMAT ),
+    lang: language,
+    limit: limit,
+  };
+
+  if( moment().isBetween( start, end ) ) {
+    // We cannot store the values forever, since we are in the timespan
+    ctx.maxAge = CACHE_MAX_AGE;
+  }
+
+
+  // Create query filter
+  const filter = {
+    lang: { $nin: [ 'und', null ] },
+  };
+  filter.$or = getDateBetween( start, end );
+
+  const allMunicipalities = _.map( MUNICIPALITIES, 'properties.PRO_COM' );
+
+
+  const anomalies = yield getAnomalies( filter, 'municipality', language );
+
+  // Get above threshold
+  const above = _.map( anomalies, 'municipality_id' );
+  response.nonTransparent = above;
+
+  // Get below threshold
+  const below = _.difference( allMunicipalities, above );
+  response.belowThreshold = below;
+
+  // Count by type
+  response.counts = _.countBy( anomalies, 'type' );
+
+  // Get nil anomalies
+  response.top = _( anomalies )
+  .orderBy( 'value', 'desc' )
+  .take( limit )
+  .value();
+
+  // Set response
+  ctx.body = response;
+}
+// Module class declaration
+
+// Module initialization (at first load)
+
+// Module exports
+module.exports = co.wrap( topDistrict );
+
+
+//  50 6F 77 65 72 65 64  62 79  56 6F 6C 6F 78
---
+++ api/municipality/tweets/district2.js
@@ -0,0 +1,118 @@
+'use strict';
+// Load system modules
+
+// Load modules
+const co = require( 'co' );
+const _ = require( 'lodash' );
+const Boom = require( 'boom' );
+const db = require( 'db-utils' );
+const debug = require( 'debug' )( 'UrbanScope:server:api:municipality:tweets:district2' );
+
+// Load my modules
+const getDateBetween = require( '../../../utils/get-between' );
+
+// Constant declaration
+const DATE_FORMAT = require( '../../../config/' ).dateFormat;
+const MUNICIPALITIES = require( '../../../config/milan_municipalities.json' );
+const CACHE_MAX_AGE = 60*60*24*1; // 1 dd
+const TIMELINE = 'timeline';
+
+// Module variables declaration
+
+// Module functions declaration
+function formatTimelineData( timeline ) {
+  return _( timeline )
+  .groupBy( 'id' )
+  .mapValues( values => {
+    return _( values )
+    .groupBy( 'lang' )
+    .mapValues( data => _.sumBy( data, 'value' ) )
+    .value();
+  } )
+  .map( ( v, k ) => {
+    return {
+      nil: Number( k ),
+      value: _( v ).map().sumBy(),
+      langs: v,
+    };
+  } )
+  .value();
+}
+function getTimelineData( filter ) {
+  return db
+  .find( TIMELINE, filter )
+  .project( {
+    _id: 0,
+    type: 0,
+  } )
+  .sort( {
+    id: 1,
+  } )
+  .toArray()
+  .then( formatTimelineData );
+}
+
+function* district( ctx ) {
+  // Cache MAX_AGE
+  ctx.maxAge = CACHE_MAX_AGE;
+
+  debug( 'Requested district' );
+
+  const start = ctx.startDate;
+  const end = ctx.endDate;
+  const language = ctx.language;
+  const nils = ctx.nils;
+
+
+  if( start.isAfter( end ) ) {
+    throw Boom.badRequest( 'Start date after end date' );
+  }
+
+  const response = {
+    startDate: start.format( DATE_FORMAT ),
+    endDate: end.format( DATE_FORMAT ),
+    language: language,
+  };
+
+
+  // Create query filter
+  const filter = {
+    lang: language,
+    type: 'municipality',
+  };
+  filter.$or = getDateBetween( start, end );
+
+
+  // Filter by language
+  if( language==='other' ) {
+    filter.lang = {
+      $nin: [ 'it', 'en', 'und', null ],
+    };
+  }
+
+
+  // Add selected nils property to the response
+  if( nils.length>0 ) {
+    filter.id = { $in: nils };
+  } else {
+    const allNils = _.map( MUNICIPALITIES, 'properties.PRO_COM' );
+    filter.id = { $in: allNils };
+  }
+
+  const data = yield getTimelineData( filter );
+
+  response.selectedNils = filter.id[ '$in' ];
+  response.nils = data;
+
+  // Set response
+  ctx.body = response;
+}
+// Module class declaration
+
+// Module initialization (at first load)
+
+// Module exports
+module.exports = co.wrap( district );
+
+
+//  50 6F 77 65 72 65 64  62 79  56 6F 6C 6F 78
